40f1a20797c98f00f461f3feff50b7790045758e
Kiel <ezekieltalampas.c@gmail.com>
2025-02-28 15:04:09 +0800

    Update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const target_bg = document.querySelector(".target_bg");
 
 let mouseX = 0, mouseY = 0;  
 let bgX = 0, bgY = 0;        
-const easeFactor = 0.03;      
+const easeFactor = 0.02;      
 
 const moveRadialBGColor = (e) => {
     mouseX = e.clientX;
@@ -21,3 +21,26 @@ const updateBackground = () => {
 
 document.addEventListener("mousemove", moveRadialBGColor, { passive: true });
 requestAnimationFrame(updateBackground);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("main .wrapper > div");
+    const navLinks = document.querySelectorAll(".sidebar-nav a");
+
+    window.addEventListener("scroll", () => {
+        let current = "";
+
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop - 100;
+            if (window.scrollY >= sectionTop) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLinks.forEach((link) => {
+            link.parentElement.classList.remove("active");
+            if (link.getAttribute("href").includes(current)) {
+                link.parentElement.classList.add("active");
+            }
+        });
+    });
+});
